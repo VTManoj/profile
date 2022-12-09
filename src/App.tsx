1d4593589934070e868e3988af8724f419afa40e
VTManoj <reachmanojvt@gmail.com>
2022-12-10 01:26:09 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import { HomePage } from './componnets/HomePage/HomePage';
 import { About } from './componnets/AboutPage/About';
